e7ae1b34b274269ba5a09cedc1a5d57d9d6660d2
John Bachir <j@jjb.cc>
2023-06-30 20:25:52 -0400

    no more globals!

--- test/lib/error_lifecycle.rb
+++ test/lib/error_lifecycle.rb
@@ -1,47 +1,49 @@
 class MyStandardError < StandardError; end
 class MyException< Exception; end
 
-def subject(error_to_raise, error_to_rescue)
-  $inner_attempted = nil
-  $inner_else = nil
-  $inner_rescue = nil
-  $inner_ensure = nil
-  $inner_ensure_has_time_to_finish = nil
+class ErrorLifeCycleTester
+  attr_reader :inner_attempted, :inner_else, :inner_rescue, :inner_ensure, :inner_ensure_has_time_to_finish, :outer_rescue, :outer_else, :outer_ensure, :outer_ensure_has_time_to_finish
 
-  $outer_rescue = nil
-  $outer_else = nil
-  $outer_ensure = nil
-  $outer_ensure_has_time_to_finish = nil
+  def subject(error_to_raise, error_to_rescue)
 
-  begin
-    Timeout.timeout(0.001, error_to_raise){
-      begin
-        $inner_attempted = true
-        nil while true
-      rescue error_to_rescue
-        $inner_rescue = true
-      else
-        $inner_else = true
-      ensure
-        $inner_ensure = true
-        t = Time.now; nil while Time.now < t+1
-        $inner_ensure_has_time_to_finish = true
-      end
-    }
-  rescue Exception
-    $outer_rescue = true
-  else
-    $outer_else = true
-  ensure
-    $outer_ensure = true
-    t = Time.now; nil while Time.now < t+1
-    $outer_ensure_has_time_to_finish = true
-  end
+    @inner_attempted = nil
+    @inner_else = nil
+    @inner_rescue = nil
+    @inner_ensure = nil
+    @inner_ensure_has_time_to_finish = nil
 
-  # remove if/when stop using global variables
-  Thread.list.each{|t| t.kill unless t==Thread.current }
+    @outer_rescue = nil
+    @outer_else = nil
+    @outer_ensure = nil
+    @outer_ensure_has_time_to_finish = nil
 
-  unless !!$outer_else ^ !!$outer_rescue
-    raise "something strange happened with the outer_rescue variables"
+    begin
+      Timeout.timeout(0.001, error_to_raise){
+        begin
+          @inner_attempted = true
+          nil while true
+        rescue error_to_rescue
+          @inner_rescue = true
+        else
+          @inner_else = true
+        ensure
+          @inner_ensure = true
+          t = Time.now; nil while Time.now < t+1
+          @inner_ensure_has_time_to_finish = true
+        end
+      }
+    rescue Exception
+      @outer_rescue = true
+    else
+      @outer_else = true
+    ensure
+      @outer_ensure = true
+      t = Time.now; nil while Time.now < t+1
+      @outer_ensure_has_time_to_finish = true
+    end
+
+    unless !!@outer_else ^ !!@outer_rescue
+      raise "something strange happened with the outer_rescue variables"
+    end
   end
 end
--- test/test_error_lifecycle.rb
+++ test/test_error_lifecycle.rb
@@ -14,100 +14,106 @@ class TestTimeout < Test::Unit::TestCase
 
   # when an exception to raise is not specified and the inner code does not catch Exception
   def test_1
-    subject(nil, StandardError)
+    s = ErrorLifeCycleTester.new
+    s.subject(nil, StandardError)
 
     # EXPECTED
-    assert $inner_attempted
-    assert !$inner_else
-    assert !$inner_rescue
-    assert $inner_ensure
-    assert $outer_rescue
-    assert $outer_ensure
-    assert $inner_ensure_has_time_to_finish
-    assert $outer_ensure_has_time_to_finish
+    assert s.inner_attempted
+    assert !s.inner_else
+    assert !s.inner_rescue
+    assert s.inner_ensure
+    assert s.outer_rescue
+    assert s.outer_ensure
+    assert s.inner_ensure_has_time_to_finish
+    assert s.outer_ensure_has_time_to_finish
   end
 
   # when an exception to raise is not specified and the inner code does catch Exception
   def test_2
-    subject(nil, Exception)
+    s = ErrorLifeCycleTester.new
+    s.subject(nil, Exception)
 
     # EXPECTED
-    assert $inner_attempted
-    assert !$inner_else
-    assert $inner_ensure
-    assert $outer_ensure
-    assert $inner_ensure_has_time_to_finish
-    assert $outer_ensure_has_time_to_finish
-    assert $inner_rescue # true in 1.9, false in gem 0.2.0, true in 0.4.0
+    assert s.inner_attempted
+    assert !s.inner_else
+    assert s.inner_ensure
+    assert s.outer_ensure
+    assert s.inner_ensure_has_time_to_finish
+    assert s.outer_ensure_has_time_to_finish
+    assert s.inner_rescue # true in 1.9, false in gem 0.2.0, true in 0.4.0
 
     # BAD?
-    assert !$outer_rescue # false in 1.9 stdlib, true in gem 0.2.0, false in 0.4.0
+    assert !s.outer_rescue # false in 1.9 stdlib, true in gem 0.2.0, false in 0.4.0
   end
 
   # when an exception to raise is StandardError and the inner code does not catch Exception
   def test_3
-    subject(MyStandardError, StandardError)
+    s = ErrorLifeCycleTester.new
+    s.subject(MyStandardError, StandardError)
 
     # EXPECTED
-    assert $inner_attempted
-    assert !$inner_else
-    assert $inner_rescue
-    assert $inner_ensure
-    assert $outer_ensure
-    assert $inner_ensure_has_time_to_finish
-    assert $outer_ensure_has_time_to_finish
+    assert s.inner_attempted
+    assert !s.inner_else
+    assert s.inner_rescue
+    assert s.inner_ensure
+    assert s.outer_ensure
+    assert s.inner_ensure_has_time_to_finish
+    assert s.outer_ensure_has_time_to_finish
 
     # BAD?
-    assert !$outer_rescue
+    assert !s.outer_rescue
   end
 
   # when an exception to raise is StandardError and the inner code does catch Exception
   def test_4
-    subject(MyStandardError, Exception)
+    s = ErrorLifeCycleTester.new
+    s.subject(MyStandardError, Exception)
 
     # EXPECTED
-    assert $inner_attempted
-    assert !$inner_else
-    assert $inner_rescue
-    assert $inner_ensure
-    assert $outer_ensure
-    assert $inner_ensure_has_time_to_finish
-    assert $outer_ensure_has_time_to_finish
+    assert s.inner_attempted
+    assert !s.inner_else
+    assert s.inner_rescue
+    assert s.inner_ensure
+    assert s.outer_ensure
+    assert s.inner_ensure_has_time_to_finish
+    assert s.outer_ensure_has_time_to_finish
 
     # BAD?
-    assert !$outer_rescue
+    assert !s.outer_rescue
   end
 
   # when an exception to raise is Exception and the inner code does not catch Exception
   def test_5
-    subject(MyException, StandardError)
+    s = ErrorLifeCycleTester.new
+    s.subject(MyException, StandardError)
 
     # EXPECTED
-    assert $inner_attempted
-    assert !$inner_else
-    assert !$inner_rescue
-    assert $inner_ensure
-    assert $outer_ensure
-    assert $outer_rescue
-    assert $inner_ensure_has_time_to_finish
-    assert $outer_ensure_has_time_to_finish
+    assert s.inner_attempted
+    assert !s.inner_else
+    assert !s.inner_rescue
+    assert s.inner_ensure
+    assert s.outer_ensure
+    assert s.outer_rescue
+    assert s.inner_ensure_has_time_to_finish
+    assert s.outer_ensure_has_time_to_finish
   end
 
   # when an exception to raise is Exception and the inner code does catch Exception
   def test_6
-    subject(MyException, Exception)
+    s = ErrorLifeCycleTester.new
+    s.subject(MyException, Exception)
 
     # EXPECTED
-    assert $inner_attempted
-    assert !$inner_else
-    assert $inner_rescue
-    assert $inner_ensure
-    assert $outer_ensure
-    assert $inner_ensure_has_time_to_finish
-    assert $outer_ensure_has_time_to_finish
+    assert s.inner_attempted
+    assert !s.inner_else
+    assert s.inner_rescue
+    assert s.inner_ensure
+    assert s.outer_ensure
+    assert s.inner_ensure_has_time_to_finish
+    assert s.outer_ensure_has_time_to_finish
 
     # BAD?
-    assert !$outer_rescue
+    assert !s.outer_rescue
   end
 
 end
